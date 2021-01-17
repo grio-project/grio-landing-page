63641ba6888bc1cd7bb7744a754b91c475d41c86
laionazeredo <laion.azeredo@gmail.com>
2021-01-17 18:11:40 -0300

    feat(React App): Implements basic app structure

---
+++ src/Components/Cover.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cover extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Cover;
---
+++ src/Components/Profiles.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profiles extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Profiles;
