bd46c1307b5f4432962d8ddfd34ff8166e538c17
Krigeren <marceloprado2610@gmail.com>
2021-01-24 19:38:45 -0300

    feat: Adicionada a seção Team.tsx (com bugs no posicionamento das colunas)

--- src/Components/Team.tsx
+++ src/Components/Team.tsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-
+import { Row, Col } from 'react-bootstrap';
 class Team extends Component {
   render() {
     return (
-      <div>
-        <ul>
-          <li>
-            Team
-          </li>
-        </ul>
+      <div className="slide">
+        <div>
+          <h1 className="title">
+            QUEM ESTÁ ENVOLVIDO?
+        </h1>
+        </div>
+        <div>
+          <Row>
+
+            <Col xs={4} >
+              <p>
+                {/* <img src={ } /> */}
+              1234
+            </p>
+            </Col>
+            <Col xs={4}>
+              <p>
+                {/* <img src={ } /> */}
+              3
+            </p>
+            </Col>
+            <Col xs={4}>
+              <p>
+                {/* <img src={ } /> */}
+              2
+            </p>
+            </Col>
+          </Row>
+        </div>
+
       </div>
     );
   }
