6ec8e53e676c0b190d9cbccfa804d0d65850b2a2
laionazeredo <laion.azeredo@gmail.com>
2021-01-24 17:11:05 -0300

    Solves merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './styles/App.css';
 import Header from './Components/Header'
 import Footer from './Components/Footer'
@@ -18,24 +17,6 @@ class App extends Component {
           <LandingPage />
           <Footer />  
         </Container>
-=======
-import logo from './logo.svg';
-import './App.css';
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import LandingPage from './Components/LandingPage'
-
-
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <LandingPage />
-        <Footer />
-        
->>>>>>> feat(React App): Implements basic app structure
       </div>
     );
   }
