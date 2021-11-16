f6a8448c476dc8e9d4c768e62de068e42c97b092
shaianer <shayarodrigues@gmail.com>
2021-11-16 11:40:49 -0300

    componente select input

---
+++ src/Components/SelectInput/index.tsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+import { SelectIputContainer } from './style'
+
+type Props = {
+    valueSelect: string, 
+    placeholder: string
+}
+
+export default function SelectInput(props: Props) {
+  const [value, setValue] = useState()  
+  const selectedValue = null;
+
+  const handleValue = (e:any) => {
+      setValue(e.target.value)
+    }
+
+  return (
+
+    <SelectIputContainer>
+        <div className="form-group">
+            <select className="form-control" >
+                <option selected>Selecione uma opção</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+    </SelectIputContainer>
+   
+  )
+}
