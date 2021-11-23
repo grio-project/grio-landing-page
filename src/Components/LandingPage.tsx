d955f3dc0d8b9f8a95ddd7462c12e68f89c6c767
Krigeren <marceloprado2610@gmail.com>
2021-11-23 16:51:30 -0300

    Ajustes e adição de uma barra de progresso

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -6,12 +6,14 @@ import Team from './Team';
 import Forms from './Forms'
 import NextSteps from './NextSteps';
 import Profiles from './Profiles';
+import Step from './Step'
 class LandingPage extends Component {
   render() {
     return (
       <Container fluid>
         
         <Forms />
+        <Step/>
         
       </Container>
     );
